1cd5e1619e026df1d5636d434e7bd9517a735879
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-12 00:35:19 +0600

    login and register page done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,68 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            Register
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen ">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-2xl font-bold">Register now!</h1>
         </div>
-    );
+        <div className="card bg-base-100 w-[400px] shadow-2xl">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+            <p className="mt-2">
+              Already Have Account? Please <Link to={"/login"}>Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
